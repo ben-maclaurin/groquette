4fc19a29b0dab51282f33c1ff0d379c8a7468c2e
Ben <ben@thedesigncollaborative.co>
2022-11-19 19:21:13 +0000

    account for whitespace before comment contents

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fmt;
 use chumsky::{combinator::Map, error::Cheap, prelude::*, primitive::Filter};
 
 // string is temporary, these need to be typed
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum Expression {
     Literal(Literal),
     SimpleExpression(SimpleExpression),
@@ -38,7 +38,7 @@ enum CompoundExpression {
     PipeFuncCall,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum OperatorCall {
     And,
     Or,
@@ -66,69 +66,17 @@ enum CommentType {
     Single,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 struct Unary<T, U> {
     operand: OperatorCall,
     unary_type: T,
     value: U,
 }
 
-impl fmt::Debug for OperatorCall {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self {
-            OperatorCall::And => todo!(),
-            OperatorCall::Or => todo!(),
-            OperatorCall::Not => todo!(),
-            OperatorCall::Equality => todo!(),
-            OperatorCall::Comparison => todo!(),
-            OperatorCall::In => todo!(),
-            OperatorCall::Match => todo!(),
-            OperatorCall::Asc => todo!(),
-            OperatorCall::Desc => todo!(),
-            OperatorCall::UnaryPlus => todo!(),
-            OperatorCall::UnaryMinus => todo!(),
-            OperatorCall::Plus => todo!(),
-            OperatorCall::Minus => todo!(),
-            OperatorCall::Star => todo!(),
-            OperatorCall::Slash => todo!(),
-            OperatorCall::Percent => todo!(),
-            OperatorCall::StarStar => todo!(),
-            OperatorCall::Comment => write!(f, "//"),
-        }
-    }
-}
-
-impl fmt::Debug for Unary<CommentType, String> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self.unary_type {
-            CommentType::Inline => write!(f, "{:?}{}", &self.operand, &self.value),
-            CommentType::Single => {
-                if &self.value.len() > &10 {
-                    write!(f, "{:?}{}", &self.operand, &self.value)
-                } else {
-                    write!(f, "{:?}{}", &self.operand, &self.value)
-                }
-            }
-        }
-    }
-}
-
-impl fmt::Debug for Expression {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self {
-            Expression::Literal(_) => todo!(),
-            Expression::SimpleExpression(_) => todo!(),
-            Expression::CompoundExpression(_) => todo!(),
-            Expression::OperatorCall(_) => todo!(),
-            Expression::Comment(c) => write!(f, "{:?}", c),
-        }
-    }
-}
-
 fn parse_comment(input: &str) -> Result<Expression, Vec<Cheap<char>>> {
     let word = any::<_, Cheap<char>>().repeated().at_least(1);
 
-    let inline_comment = take_until(just("//"))
+    let inline_comment = take_until(just("// "))
         .ignored()
         .then(take_until(text::newline()))
         .map(|c| {
@@ -141,7 +89,7 @@ fn parse_comment(input: &str) -> Result<Expression, Vec<Cheap<char>>> {
             })
         });
 
-    let single_line_comment = just::<_, _, Cheap<char>>("//")
+    let single_line_comment = just::<_, _, Cheap<char>>("// ")
         .then(take_until(text::newline()))
         .map(|c| {
             let (value, _) = c.1;
@@ -175,7 +123,9 @@ mod tests {
 
     #[test]
     fn can_parse_single_line_comment() {
-        let single_line_comment = "// single line comment";
+        let single_line_comment = r#"
+	    // single line comment
+	"#;
 
         assert_eq!(
             parse_comment(single_line_comment).unwrap(),
@@ -189,7 +139,9 @@ mod tests {
 
     #[test]
     fn can_parse_inline_comment() {
-        let inline_comment = "text // inline comment";
+        let inline_comment = r#"
+	    *[name == "Michael"] // inline comment
+	"#;
 
         assert_eq!(
             parse_comment(inline_comment).unwrap(),
